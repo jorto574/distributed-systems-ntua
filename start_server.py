d8465f9874bb1428894e20faea93c178928dec56
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2024-03-26 00:45:47 +0200

    Removed start_server.py and made work on port mapping with Dockers

--- start_server.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-if __name__ == "__main__":
-    try:
-        os.chdir("server")
-        subprocess.run(["python3", "app.py"])
-    except:
-        print("Internal error! 'server/' directory not found.")
